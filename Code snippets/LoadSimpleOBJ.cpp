043ebf8b0be1e23420ee5636e49179a2a03e4d0e
gsbado <gabibado92@gmail.com>
2026-05-07 18:20:49 -0300

    feat: update loadSimpleOBJ to support texture coordinates

--- Code snippets/LoadSimpleOBJ.cpp
+++ Code snippets/LoadSimpleOBJ.cpp
@@ -108,6 +108,8 @@ int loadSimpleOBJ(string filePATH, int &nVertices)
                 vBuffer.push_back(color.r);
                 vBuffer.push_back(color.g);
                 vBuffer.push_back(color.b);
+                vBuffer.push_back(texCoords[ti].x);
+                vBuffer.push_back(texCoords[ti].y);
             }
         }
     }
@@ -123,16 +125,19 @@ int loadSimpleOBJ(string filePATH, int &nVertices)
     glGenVertexArrays(1, &VAO);
     glBindVertexArray(VAO);
     
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (GLvoid*)0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), (GLvoid*)0);
     glEnableVertexAttribArray(0);
 
-    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (GLvoid*)(3 * sizeof(GLfloat)));
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), (GLvoid*)(3 * sizeof(GLfloat)));
     glEnableVertexAttribArray(1);
+
+    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), (GLvoid*)(6 * sizeof(GLfloat)));
+    glEnableVertexAttribArray(2);
     
     glBindBuffer(GL_ARRAY_BUFFER, 0);
     glBindVertexArray(0);
 
-	nVertices = vBuffer.size() / 6;  // x, y, z, r, g, b (valores atualmente armazenados por vértice)
+	nVertices = vBuffer.size() / 8;  // x, y, z, r, g, b, u, v (valores atualmente armazenados por vértice)
 
     return VAO;
 }
