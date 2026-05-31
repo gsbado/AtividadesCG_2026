1cb9031cb6e01677990444f59538b53a434b812b
gsbado <gabibado92@gmail.com>
2026-05-31 18:58:14 -0300

    feat: add vertex normal support to OBJ loading

--- Code snippets/LoadSimpleOBJ.cpp
+++ Code snippets/LoadSimpleOBJ.cpp
@@ -23,35 +23,30 @@
  *
  */
 
- // Cabeçalhos necessários (para esta função), acrescentar ao seu código 
+// Cabeçalhos necessários (para esta função), acrescentar ao seu código
 #include <iostream>
 #include <fstream>
 #include <sstream>
 #include <string>
 #include <vector>
- 
- 
 using namespace std;
 
 // GLAD
 #include <glad/glad.h>
- 
 // GLFW
 #include <GLFW/glfw3.h>
- 
-//GLM
+// GLM
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
 
-struct Mesh 
+struct Mesh
 {
-    GLuint VAO; 
-
+    GLuint VAO;
 };
 
 int loadSimpleOBJ(string filePATH, int &nVertices)
- {
+{
     std::vector<glm::vec3> vertices;
     std::vector<glm::vec2> texCoords;
     std::vector<glm::vec3> normals;
@@ -59,48 +54,51 @@ int loadSimpleOBJ(string filePATH, int &nVertices)
     glm::vec3 color = glm::vec3(1.0, 0.0, 0.0);
 
     std::ifstream arqEntrada(filePATH.c_str());
-    if (!arqEntrada.is_open()) 
-	{
+    if (!arqEntrada.is_open())
+    {
         std::cerr << "Erro ao tentar ler o arquivo " << filePATH << std::endl;
         return -1;
     }
 
     std::string line;
-    while (std::getline(arqEntrada, line)) 
-	{
+    while (std::getline(arqEntrada, line))
+    {
         std::istringstream ssline(line);
         std::string word;
         ssline >> word;
 
-        if (word == "v") 
-		{
+        if (word == "v")
+        {
             glm::vec3 vertice;
             ssline >> vertice.x >> vertice.y >> vertice.z;
             vertices.push_back(vertice);
-        } 
-        else if (word == "vt") 
-		{
+        }
+        else if (word == "vt")
+        {
             glm::vec2 vt;
             ssline >> vt.s >> vt.t;
             texCoords.push_back(vt);
-        } 
-        else if (word == "vn") 
-		{
+        }
+        else if (word == "vn")
+        {
             glm::vec3 normal;
             ssline >> normal.x >> normal.y >> normal.z;
             normals.push_back(normal);
-        } 
+        }
         else if (word == "f")
-		 {
-            while (ssline >> word) 
-			{
+        {
+            while (ssline >> word)
+            {
                 int vi = 0, ti = 0, ni = 0;
                 std::istringstream ss(word);
                 std::string index;
 
-                if (std::getline(ss, index, '/')) vi = !index.empty() ? std::stoi(index) - 1 : 0;
-                if (std::getline(ss, index, '/')) ti = !index.empty() ? std::stoi(index) - 1 : 0;
-                if (std::getline(ss, index)) ni = !index.empty() ? std::stoi(index) - 1 : 0;
+                if (std::getline(ss, index, '/'))
+                    vi = !index.empty() ? std::stoi(index) - 1 : 0;
+                if (std::getline(ss, index, '/'))
+                    ti = !index.empty() ? std::stoi(index) - 1 : 0;
+                if (std::getline(ss, index))
+                    ni = !index.empty() ? std::stoi(index) - 1 : 0;
 
                 vBuffer.push_back(vertices[vi].x);
                 vBuffer.push_back(vertices[vi].y);
@@ -110,6 +108,9 @@ int loadSimpleOBJ(string filePATH, int &nVertices)
                 vBuffer.push_back(color.b);
                 vBuffer.push_back(texCoords[ti].x);
                 vBuffer.push_back(texCoords[ti].y);
+                vBuffer.push_back(normals[ni].x);
+                vBuffer.push_back(normals[ni].y);
+                vBuffer.push_back(normals[ni].z);
             }
         }
     }
@@ -121,23 +122,50 @@ int loadSimpleOBJ(string filePATH, int &nVertices)
     glGenBuffers(1, &VBO);
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, vBuffer.size() * sizeof(GLfloat), vBuffer.data(), GL_STATIC_DRAW);
-    
+
     glGenVertexArrays(1, &VAO);
     glBindVertexArray(VAO);
-    
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), (GLvoid*)0);
+
+    glVertexAttribPointer(
+        0,
+        3,
+        GL_FLOAT,
+        GL_FALSE,
+        11 * sizeof(GLfloat),
+        (GLvoid *)0);
     glEnableVertexAttribArray(0);
 
-    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), (GLvoid*)(3 * sizeof(GLfloat)));
+    glVertexAttribPointer(
+        1,
+        3,
+        GL_FLOAT,
+        GL_FALSE,
+        11 * sizeof(GLfloat),
+        (GLvoid *)(3 * sizeof(GLfloat)));
     glEnableVertexAttribArray(1);
 
-    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), (GLvoid*)(6 * sizeof(GLfloat)));
+    glVertexAttribPointer(
+        2,
+        2,
+        GL_FLOAT,
+        GL_FALSE,
+        11 * sizeof(GLfloat),
+        (GLvoid *)(6 * sizeof(GLfloat)));
     glEnableVertexAttribArray(2);
-    
+
+    glVertexAttribPointer(
+        3,
+        3,
+        GL_FLOAT,
+        GL_FALSE,
+        11 * sizeof(GLfloat),
+        (GLvoid *)(8 * sizeof(GLfloat)));
+    glEnableVertexAttribArray(3);
+
     glBindBuffer(GL_ARRAY_BUFFER, 0);
     glBindVertexArray(0);
 
-	nVertices = vBuffer.size() / 8;  // x, y, z, r, g, b, u, v (valores atualmente armazenados por vértice)
+    nVertices = vBuffer.size() / 11; // x, y, z, r, g, b, u, v, nx, ny, nz
 
     return VAO;
 }
