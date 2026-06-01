71c0dcb9f2def1c861bbffec1be46f6530dc7c64
gsbado <gabibado92@gmail.com>
2026-05-31 21:10:34 -0300

    refactor: adjust glClearColor values for better contrast

--- src/Desafios/M4/HelloIlumination3D.cpp
+++ src/Desafios/M4/HelloIlumination3D.cpp
@@ -342,7 +342,7 @@ GLuint loadTexture(
 
 void prepareFrame()
 {
-	glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+	glClearColor(0.05f, 0.05f, 0.05f, 1.0f);
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 }
 
